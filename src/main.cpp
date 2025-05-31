80bd1db1a3c9f7fed2daea28f3d978d21f548c29
nilswag <nilswagemaker05@icloud.com>
2025-05-31 16:36:27 +0200

    added some rendering code

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,39 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+float vertices[6] = {
+	-0.5f, -0.5f,
+	 0.0f,  0.5f,
+	 0.5f, -0.5f
+};
+
 int main(void) {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	if (!glfwInit()) std::cout << "failed to initialize glfw" << std::endl;
+
 	GLFWwindow* window = nullptr;
 	window = glfwCreateWindow(1280, 720, "testing", nullptr, nullptr);
 	if (window == nullptr) std::cout << "failed to create glfw window" << std::endl;
+
 	glfwMakeContextCurrent(window);
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) std::cout << "failed to initialize glad" << std::endl;
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), vertices, GL_STATIC_DRAW);
+
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
+
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
 	}
+
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
