262454adb4630d59ce88d886acd0b55eb9804d49
nilswag <nilswagemaker05@icloud.com>
2025-06-23 14:25:55 +0200

    Added basic opengl boilerplate

--- src/main.c
+++ src/main.c
@@ -1,9 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <stdio.h>
+
+float vertices[] = {
+    -0.5f, -0.5f,
+     0.0f,  0.5f,
+     0.5f, -0.5f
+};
 
 int main(void)
 {
-    printf("Hello World!\n");
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    
+    if (!glfwInit())
+    {
+        fputs("Failed to initialize GLFW.", stderr);
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "glfw window", NULL, NULL);
+    if (window == NULL)
+    {
+        fputs("Failed to initialize GLFW window.", stderr);
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        fputs("Failed to initialize glad.", stderr);
+        glfwTerminate();
+        return -1;
+    }
+    glViewport(0, 0, 1280, 720);
+
+    // ======================================
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // ======================================
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
