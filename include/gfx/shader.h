ed89f05171a4e68f4ac9282eeaadc133adfc6e88
nilswag <nilswagemaker05@icloud.com>
2025-06-25 17:17:51 +0200

    Integrated uniform hash map

--- include/gfx/shader.h
+++ include/gfx/shader.h
@@ -1,22 +1,6 @@
 #pragma once
 #include <glad/glad.h>
 
-typedef struct
-{
-    GLuint id;
-    GLuint vertex_id;
-    GLuint fragment_id;
-} shader_t;
-
-shader_t gfx_shader_init(const char* vertex_src, const char* fragment_src);
-void gfx_shader_use(shader_t* shader);
-void gfx_shader_free(shader_t* shader);
-
-void gfx_shader_set_int(shader_t* shader, const char* name, int value);
-void gfx_shader_set_float(shader_t* shader, const char* name, float value);
-void gfx_shader_set_bool(shader_t* shader, const char* name, int value);
-
-
 typedef struct entry_t
 {
     const char* key;
@@ -37,3 +21,20 @@ void gfx_uniform_map_put(uniform_map_t* map, const char* key, GLint value);
 GLint gfx_uniform_map_get(uniform_map_t* map, const char* key);
 void gfx_uniform_map_remove(uniform_map_t* map, const char* key);
 void gfx_uniform_map_clear(uniform_map_t* map);
+
+
+typedef struct
+{
+    GLuint id;
+    GLuint vertex_id;
+    GLuint fragment_id;
+    uniform_map_t* uniform_map;
+} shader_t;
+
+shader_t gfx_shader_init(const char* vertex_src, const char* fragment_src);
+void gfx_shader_use(shader_t* shader);
+void gfx_shader_free(shader_t* shader);
+
+void gfx_shader_set_int(shader_t* shader, const char* name, int value);
+void gfx_shader_set_float(shader_t* shader, const char* name, float value);
+void gfx_shader_set_bool(shader_t* shader, const char* name, int value);
--- src/gfx/shader.c
+++ src/gfx/shader.c
@@ -7,108 +7,6 @@
 #include "util/util.h"
 #include "gfx/shader.h"
 
-static GLuint compile_shader(const char* shader_src, GLenum type)
-{
-    GLuint id = glCreateShader(type);
-    glShaderSource(id, 1, &shader_src, NULL);
-    glCompileShader(id);
-
-    GLuint success = 1;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        GLsizei length = 0;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-        char* log = malloc(length * sizeof(char));
-        glGetShaderInfoLog(id, length, NULL, log);
-        fprintf(stderr, "Failed to compile shader: \n%s", log);
-        free(log);
-        glDeleteShader(id);
-        return 0;
-    }
-
-    return id;
-}
-
-shader_t gfx_shader_init(const char* vertex_src, const char* fragment_src)
-{
-    shader_t s = { 0 };
-    s.id = glCreateProgram();
-    s.vertex_id = compile_shader(vertex_src, GL_VERTEX_SHADER);
-    s.fragment_id = compile_shader(fragment_src, GL_FRAGMENT_SHADER);
-
-    if (!s.vertex_id || !s.fragment_id)
-    {
-        fputs("Aborting shader creation due to shader compilation failure.\n", stderr);
-        return s;
-    }
-
-    glAttachShader(s.id, s.vertex_id);
-    glAttachShader(s.id, s.fragment_id);
-    glLinkProgram(s.id);
-
-    GLuint success = 1;
-    glGetProgramiv(s.id, GL_LINK_STATUS, &success);
-    if (!success)
-    {
-        GLsizei length = 0;
-        glGetProgramiv(s.id, GL_INFO_LOG_LENGTH, &length);
-        char* log = malloc(length * sizeof(char));
-        glGetProgramInfoLog(s.id, length, NULL, log);
-        fprintf(stderr, "Failed to link shader program: \n%s", log);
-        free(log);
-    }
-
-    return s;
-}
-
-void gfx_shader_use(shader_t* shader)
-{
-    glUseProgram(shader->id);
-}
-
-void gfx_shader_free(shader_t* shader)
-{
-    glDeleteShader(shader->vertex_id);
-    glDeleteShader(shader->fragment_id);
-    glDeleteProgram(shader->id);
-}
-
-void gfx_shader_set_int(shader_t* shader, const char* name, int value)
-{
-    GLint location = glGetUniformLocation(shader->id, name);
-    glUniform1i(location, value);
-}
-
-void gfx_shader_set_float(shader_t* shader, const char* name, float value)
-{
-    GLint location = glGetUniformLocation(shader->id, name);
-    glUniform1f(location, value);
-}
-
-void gfx_shader_set_bool(shader_t* shader, const char* name, int value)
-{
-    gfx_shader_set_int(shader, name, value);
-}
-
-
-uniform_map_t* gfx_uniform_map_init(void)
-{
-    uniform_map_t* map = malloc(sizeof(uniform_map_t));
-    if (map == NULL)
-    {
-        fputs("Failed to allocate memory for uniform map.", stderr);
-        return NULL;
-    }
-
-    map->size = 0;
-    map->capacity = 100;
-    map->entries = calloc(map->capacity, sizeof(entry_t));
-
-    return map;
-}
-
-
 static void uniform_map_resize(uniform_map_t* map, size_t new_capacity)
 {
     if (new_capacity < 1) return;
@@ -136,6 +34,22 @@ static void uniform_map_resize(uniform_map_t* map, size_t new_capacity)
     free(map->entries);
     map->entries = tmp;
     map->capacity = new_capacity;
+}
+
+uniform_map_t* gfx_uniform_map_init(void)
+{
+    uniform_map_t* map = malloc(sizeof(uniform_map_t));
+    if (map == NULL)
+    {
+        fputs("Failed to allocate memory for uniform map.", stderr);
+        return NULL;
+    }
+
+    map->size = 0;
+    map->capacity = 100;
+    map->entries = calloc(map->capacity, sizeof(entry_t));
+
+    return map;
 }
 
 void gfx_uniform_map_free(uniform_map_t* map)
@@ -215,4 +129,105 @@ void gfx_uniform_map_clear(uniform_map_t* map)
 {
     for (int i = 0; i < map->capacity; i++)
         map->entries[i] = NULL;
+}
+
+
+static GLuint compile_shader(const char* shader_src, GLenum type)
+{
+    GLuint id = glCreateShader(type);
+    glShaderSource(id, 1, &shader_src, NULL);
+    glCompileShader(id);
+
+    GLuint success = 1;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        GLsizei length = 0;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* log = malloc(length * sizeof(char));
+        glGetShaderInfoLog(id, length, NULL, log);
+        fprintf(stderr, "Failed to compile shader: \n%s", log);
+        free(log);
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+shader_t gfx_shader_init(const char* vertex_src, const char* fragment_src)
+{
+    shader_t s = { 0 };
+    s.id = glCreateProgram();
+    s.vertex_id = compile_shader(vertex_src, GL_VERTEX_SHADER);
+    s.fragment_id = compile_shader(fragment_src, GL_FRAGMENT_SHADER);
+    s.uniform_map = gfx_uniform_map_init();
+
+    if (!s.vertex_id || !s.fragment_id)
+    {
+        fputs("Aborting shader creation due to shader compilation failure.\n", stderr);
+        return s;
+    }
+
+    glAttachShader(s.id, s.vertex_id);
+    glAttachShader(s.id, s.fragment_id);
+    glLinkProgram(s.id);
+
+    GLint success = 1;
+    glGetProgramiv(s.id, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        GLsizei length = 0;
+        glGetProgramiv(s.id, GL_INFO_LOG_LENGTH, &length);
+        char* log = malloc(length * sizeof(char));
+        glGetProgramInfoLog(s.id, length, NULL, log);
+        fprintf(stderr, "Failed to link shader program: \n%s", log);
+        free(log);
+    }
+
+    GLint active_uniforms = 0;
+    glGetProgramiv(s.id, GL_ACTIVE_UNIFORMS, &active_uniforms);
+    GLint uniform_max_length = 0;
+    glGetProgramiv(s.id, GL_ACTIVE_UNIFORM_MAX_LENGTH, &uniform_max_length);
+    char* name = malloc(uniform_max_length);
+    for (int i = 0; i < active_uniforms; i++)
+    {
+        glGetActiveUniform(s.id, i, uniform_max_length, NULL, NULL, NULL, name);
+        GLint location = glGetUniformLocation(s.id, name);
+        gfx_uniform_map_put(s.uniform_map, name, location);
+    }
+
+    return s;
+}
+
+void gfx_shader_use(shader_t* shader)
+{
+    glUseProgram(shader->id);
+}
+
+void gfx_shader_free(shader_t* shader)
+{
+    glDeleteShader(shader->vertex_id);
+    glDeleteShader(shader->fragment_id);
+    glDeleteProgram(shader->id);
+    gfx_uniform_map_free(shader->uniform_map);
+}
+
+void gfx_shader_set_int(shader_t* shader, const char* name, int value)
+{
+    entry_t* entry = gfx_uniform_map_get(shader->uniform_map, name);
+    gfx_shader_use(shader);
+    glUniform1i(entry->value, value);
+}
+
+void gfx_shader_set_float(shader_t* shader, const char* name, float value)
+{
+    entry_t* entry = gfx_uniform_map_get(shader->uniform_map, name);
+    gfx_shader_use(shader);
+    glUniform1f(entry->value, value);
+}
+
+void gfx_shader_set_bool(shader_t* shader, const char* name, int value)
+{
+    gfx_shader_set_int(shader, name, value);
 }
