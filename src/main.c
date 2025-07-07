bab9927d2b23bec61fe8e623dba3417ce0e2b6d0
nilswag <nilswagemaker05@icloud.com>
2025-07-08 01:49:51 +0200

    Corrected glfwWindowHint location

--- src/main.c
+++ src/main.c
@@ -36,16 +36,16 @@ static void _framebuffer_size_callback(GLFWwindow* window, int width, int height
 
 int main(void)
 {
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    
     if (!glfwInit())
     {
         fputs("Failed to initialize GLFW.", stderr);
         return -1;
     }
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
     GLFWwindow* window = glfwCreateWindow(1280, 720, "0 FPS", NULL, NULL);
     if (window == NULL)
