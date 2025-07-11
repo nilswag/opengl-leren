5e44fda72b6c46d1c0e659cd17b595f97e3b503c
nilswag <nilswagemaker05@icloud.com>
2025-07-11 18:23:35 +0200

    Small change

--- src/main.c
+++ src/main.c
@@ -1,5 +1,32 @@
+#include <stdio.h>
 #include "gfx/window.h"
 
+
+static void init(void)
+{
+
+}
+
+
+static void tick(float delta)
+{
+
+}
+
+
+static void render(void)
+{
+
+}
+
+
+int main(void)
+{
+    window_t window = { 0 };
+    gfx_window_init(&window, (window_callbacks_t) { init, tick, render });
+    gfx_window_loop(&window);
+    return 0;
+}
 
 static void init(void)
 {
