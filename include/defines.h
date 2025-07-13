bc81213ee1f17ccb35421e43b95f6ba8a8febe6b
nilswag <nilswagemaker05@icloud.com>
2025-07-13 22:25:28 +0200

    Added some typedefs for standard definitions

---
+++ include/defines.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <stdbool.h>
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+
+typedef signed char s8;
+typedef signed short s16;
+typedef signed int s32;
+typedef signed long long s64;
+
+typedef char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
