23a34f2b792d4044f6015bc0a6f71baad16dd1cc
nilswag <nilswagemaker05@icloud.com>
2025-07-13 23:01:26 +0200

    Applied logging macros

--- src/gfx/window.c
+++ src/gfx/window.c
@@ -3,16 +3,14 @@
 #include <GLFW/glfw3.h>
 
 #include "gfx/window.h"
+#include "defines.h"
+#include "util/log.h"
 
 
 static void _framebuffer_size_callback(GLFWwindow* handle, int width, int height)
 {
     window_t* window = (window_t*)glfwGetWindowUserPointer(handle);
-    if (!window)
-    {
-        fputs("Failed to retrieve GLFW window user pointer.", stderr);
-        return;
-    }
+    if (!window) LOG_FATAL("Failed to retrieve GLFW window user pointer.");
     glViewport(0, 0, width, height);
     window->width = width;
     window->height = height;
@@ -21,21 +19,13 @@ static void _framebuffer_size_callback(GLFWwindow* handle, int width, int height
 
 void gfx_window_init(window_t* window, window_callbacks_t callbacks)
 {
-    if (!glfwInit())
-    {
-        fputs("Failed to initialize GLFW.", stderr);
-        return;
-    }
+    if (!glfwInit()) LOG_FATAL("Failed to initialize GLFW.");
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* handle = glfwCreateWindow(800, 800, "0 FPS", NULL, NULL);
-    if (handle == NULL)
-    {
-        fputs("Failed to initialize GLFW window.", stderr);
-        return;
-    }
+    if (handle == NULL) LOG_FATAL("Failed to initialize GLFW window.");
     glfwMakeContextCurrent(handle);
     glfwSwapInterval(1);
     window->handle = handle;
@@ -46,11 +36,7 @@ void gfx_window_init(window_t* window, window_callbacks_t callbacks)
     glfwSetWindowUserPointer(handle, window);
     glfwSetFramebufferSizeCallback(handle, _framebuffer_size_callback);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        fputs("Failed to initialize glad.", stderr);
-        return;
-    }
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) LOG_FATAL("Failed to initialize glad.");
     glViewport(0, 0, 800, 800);
 }
 
