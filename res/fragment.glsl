1ff26c0d80e16f403be8b9a98acc79bc014ab6f4
nilswag <nilswagemaker05@icloud.com>
2026-01-25 15:12:51 +0100

    Tried out some uniform stuff

--- res/fragment.glsl
+++ res/fragment.glsl
@@ -1,9 +1,10 @@
 #version 460 core
 
 out vec4 color;
-in vec3 _color;
+
+uniform vec4 _color;
 
 void main()
 {
-    color = vec4(_color, 1.0);
+    color = _color;
 }
--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 #include "state.h"
 #include "gfx/shader.h"
 
+#include <math.h>
+
 State state = { 0 };
 
 static void _framebuffer_size_callback(GLFWwindow* window, i32 width, i32 height)
@@ -16,9 +18,9 @@ static void _framebuffer_size_callback(GLFWwindow* window, i32 width, i32 height
 }
 
 f32 vertices[] = {
-    -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-     0.0f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-     0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+    -0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f
 };
 
 int main()
@@ -52,13 +54,12 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(f32), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(f32), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(f32), (void*)(3 * sizeof(f32)));
-    glEnableVertexAttribArray(1);
 
     u32 program = create_shader("vertex.glsl", "fragment.glsl");
     glUseProgram(program);
+    i32 color_location = glGetUniformLocation(program, "_color");
 
     state.running = true;
     while (state.running && !glfwWindowShouldClose(state.window))
@@ -68,6 +69,9 @@ int main()
         double first = glfwGetTime();
         state.dt = first - last;
         last = first;
+
+        f32 value = sin(glfwGetTime());
+        glUniform4f(color_location, 0.0f, value, 0.0f, 1.0f);
 
         glClearColor(.0f, .0f, .0f, .0f);
         glClear(GL_COLOR_BUFFER_BIT);
