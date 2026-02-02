9b62cb96dc6bee3144cec9ce331f4469aba02885
nilswag <nilswagemaker05@icloud.com>
2026-02-02 22:56:54 +0100

    Tested if older opengl version would work

--- src/gfx/window.c
+++ src/gfx/window.c
@@ -22,8 +22,8 @@ void window_init(struct window* w)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #else
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #endif
 
