fabf8ff9e2001e01bdbadcb2f7be0fcaa8adfafa
nilswag <nilswagemaker05@icloud.com>
2026-02-03 22:08:49 +0100

    Refactored camera.h and camera.c with typedefs

--- src/gfx/camera/camera.c
+++ src/gfx/camera/camera.c
@@ -1,14 +1,13 @@
 #include "camera.h"
-#include "math/linmath.h"
 
-void camera_init(struct camera* c, vec2 screen_size)
+void camera_init(Camera* c, vec2 screen_size)
 {
     c->zoom = 1.0f;
     mat3f_ortho(c->proj, 0, screen_size[0], 0, screen_size[1]);
     mat3f_identity(c->view);
 }
 
-void camera_update(struct camera* c)
+void camera_update(Camera* c)
 {
     mat3f_transform(c->view, (vec2f) { -c->position[0], -c->position[1] }, (vec2f) { c->zoom, c->zoom }, 0.0f);
 }
--- src/gfx/camera/camera.h
+++ src/gfx/camera/camera.h
@@ -2,14 +2,14 @@
 #include "math/linmath.h"
 #include "util/core/defines.h"
 
-struct camera
+typedef struct
 {
     vec2f position;
     f32 zoom;
     mat3f view;
     mat3f proj;
-};
+} Camera;
 
-void camera_init(struct camera* c, vec2 window_size);
+void camera_init(Camera* c, vec2 window_size);
 
-void camera_update(struct camera* c);
+void camera_update(Camera* c);
