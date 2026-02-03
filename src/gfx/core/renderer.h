5bcd04697ef9b6cb308101ca9714a02ebb09cde0
nilswag <nilswagemaker05@icloud.com>
2026-02-03 22:04:02 +0100

    Refactored renderer.h, added typedefs

--- src/gfx/core/renderer.h
+++ src/gfx/core/renderer.h
@@ -5,41 +5,39 @@
 #define MAX_QUADS 256
 #define INSTANCE_SIZE 13
 
-enum pass_type
+typedef enum
 {
     PASS_WORLD,
     N_PASSES
-};
+} PassType;
 
-struct quad
+typedef struct
 {
     vec2f pos;
     vec2f size;
     f32 rot;
     vec4f color;
-};
+} Quad;
 
-struct render_pass
+typedef struct
 {
     u32 shader;
     u32 vao, instance_vbo;
     u64 count;
     f32 queue[MAX_QUADS][INSTANCE_SIZE];
-};
+} RenderPass;
 
-struct renderer
+typedef struct
 {
-    struct render_pass passes[N_PASSES];
-};
+    RenderPass passes[N_PASSES];
+} Renderer;
 
-void renderer_init(struct renderer* r);
-void renderer_deinit(struct renderer* r);
+void renderer_init(Renderer* r);
 
-void renderer_begin(struct renderer* r);
-void renderer_submit(struct renderer* r, enum pass_type pass, struct quad instance);
-void renderer_flush_pass(struct renderer* r, enum pass_type pass);
-void renderer_end(struct renderer* r);
+void renderer_begin(Renderer* r);
+void renderer_submit(Renderer* r, PassType pass, Quad instance);
+void renderer_flush_pass(Renderer* r, PassType pass);
+void renderer_end(Renderer* r);
 
-struct camera;
-
-void renderer_set_camera(struct renderer* r, enum pass_type pass, struct camera* camera);
+typedef struct camera Camera; // forward declaration
+void renderer_set_camera(Renderer* r, PassType pass, Camera* camera);
